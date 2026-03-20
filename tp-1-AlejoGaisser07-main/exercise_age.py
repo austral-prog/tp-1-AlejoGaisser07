74c8a42f4c1456304b2d0ac339a8ed534f62fbe8
AlejoGaisser07 <agaisser@mail.austral.edu.ar>
2026-03-20 12:27:32 -0300

    Add files via upload

--- tp-1-AlejoGaisser07-main/exercise_age.py
+++ tp-1-AlejoGaisser07-main/exercise_age.py
@@ -8,14 +8,14 @@ def age():
     3. La edad en horas (1 día = 24 horas)
     4. La edad en minutos (1 hora = 60 minutos)
     """
-    edad_años = 25
+    edad_anos = 25
     año_meses = 12
     año_dias = 365
     dia_horas = 24
     hora_minutos = 60
-    edad_meses = edad_años * año_meses
+    edad_meses = edad_anos * año_meses
     print(edad_meses)
-    edad_dias = edad_años * año_dias
+    edad_dias = edad_anos * año_dias
     print(edad_dias)
     edad_horas = edad_dias * dia_horas
     print(edad_horas)
